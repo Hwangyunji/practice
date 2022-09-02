c71d0809c91a10bd7a21c50b7c339ce8af70c676
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2022-09-02 16:38:40 +0900

    Add files via upload

---
+++ 연습/main.js
@@ -0,0 +1,78 @@
+// $(function(){
+//     const chart1 = document.querySelector('doughnut1');
+//     const chart2 = document.querySelector('doughnut2');
+//     const chart3 = document.querySelector('doughnut3');
+
+//     const makeChart = (percent, classname, color) => {
+//         let i = 1;
+//         let chartFn = setInterval(function(){
+//             if (i <= percent) {
+//                 colorFn(i, classname, color);
+//                 i++;
+//             } else {
+//                 clearInterval(chartFn);
+//             }
+//         }, 10);
+//     }
+
+//     const colorFn = (i, classname, color) => {
+//         classname.style.background = "conic-gradient(" +color+ " 0% " + i + "%, #dedede " + i + "% 100%";
+//     }
+
+//     const replay = () => {
+//         makeChart(80, chart1, '#f5b914');
+//         makeChart(40, chart2, '#0a174e');
+//         makeChart(60, chart3, '#66d2ce');
+//     }
+//     makeChart(80, chart1, '#f5b914');
+//         makeChart(40, chart2, '#0a174e');
+//         makeChart(60, chart3, '#66d2ce');
+// })
+
+
+
+
+$(document).ready(function () {
+    $(".orange-paint").click(function () {
+        $(".text1").toggleClass("orange")
+    });
+
+    $(".pink-paint").click(function () {
+        $(".text1").toggleClass("pink")
+    });
+});
+
+
+
+
+$(function () {
+    const chart1 = document.querySelector('.doughnut1');
+    const chart2 = document.querySelector('.doughnut2');
+    const chart3 = document.querySelector('.doughnut3');
+
+    const makeChart = (percent, classname, color) => {
+        let i = 1;
+        let chartFn = setInterval(function () {
+            if (i < percent) {
+                colorFn(i, classname, color);
+                i++;
+            } else {
+                clearInterval(chartFn);
+            }
+        }, 10);
+    }
+
+    const colorFn = (i, classname, color) => {
+        classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #dedede " + i + "% 100%)";
+    }
+
+    const replay = () => {
+        makeChart(80, chart1, '#f5b914');
+        makeChart(50, chart2, '#0A174E');
+        makeChart(30, chart3, '#66d2ce');
+    }
+
+    makeChart(80, chart1, '#f5b914');
+    makeChart(50, chart2, '#0A174E');
+    makeChart(30, chart3, '#66d2ce');
+})
